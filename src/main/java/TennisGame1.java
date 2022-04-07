a6960afc61d48a5664cefb3ccbe4ee56feee4085
Scott Hammer <scott.a.hammer@outlook.com>
2022-04-07 14:19:18 -0400

    SH Renamed minusResult to scoreDiff

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -27,10 +27,10 @@ public class TennisGame1 implements TennisGame {
         }
         else if (scoreIsLateInGame(m_score1, m_score2))
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
+            int scoreDiff = m_score1-m_score2;
+            if (scoreDiff==1) score ="Advantage player1";
+            else if (scoreDiff ==-1) score ="Advantage player2";
+            else if (scoreDiff>=2) score = "Win for player1";
             else score ="Win for player2";
         }
         else
