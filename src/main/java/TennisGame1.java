d861f1fe62dc93a52e4c53dc860cff77138f5eaf
Scott Hammer <scott.a.hammer@outlook.com>
2022-04-07 16:41:42 -0400

    SH Removed unnecessary boxing of ints

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -31,30 +31,30 @@ public class TennisGame1 implements TennisGame {
     }
 
     private static final Map<Integer, String> TIE_SCORE_MAP = new HashMap<Integer, String>(){{
-        put(Integer.valueOf(0), "Love-All");
-        put(Integer.valueOf(1), "Fifteen-All");
-        put(Integer.valueOf(2), "Thirty-All");
-        put(Integer.valueOf(3), "Deuce");
+        put(0, "Love-All");
+        put(1, "Fifteen-All");
+        put(2, "Thirty-All");
+        put(3, "Deuce");
     }};
 
     private String computeTiedScore(int score1) {
         if (score1 < 3)
-            return TIE_SCORE_MAP.get(Integer.valueOf(score1));
+            return TIE_SCORE_MAP.get(score1);
         else
-            return TIE_SCORE_MAP.get(Integer.valueOf(3));
+            return TIE_SCORE_MAP.get(3);
     }
 
     private static final Map<Integer, String> EARLY_SCORE_MAP = new HashMap<Integer, String>(){{
-        put(Integer.valueOf(0), "Love");
-        put(Integer.valueOf(1), "Fifteen");
-        put(Integer.valueOf(2), "Thirty");
-        put(Integer.valueOf(3), "Forty");
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
     }};
 
     private String computeEarlyGameScore(int score1, int score2) {
-        return EARLY_SCORE_MAP.get(Integer.valueOf(score1))
+        return EARLY_SCORE_MAP.get(score1)
                 + "-"
-                + EARLY_SCORE_MAP.get(Integer.valueOf(score2));
+                + EARLY_SCORE_MAP.get(score2);
     }
 
     private String computeLateGameScore(int score1, int score2) {
